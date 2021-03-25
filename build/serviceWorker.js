0842c7c74b894e513ab42a668f2f1ae4b4e5c8df
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2021-03-25 09:30:31 +0000

    polish up service worker

--- build/serviceWorker.js
+++ build/serviceWorker.js
@@ -1,12 +1,9 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
-            .then(() => {
-                return navigator.serviceWorker.ready;
-            })
-            .then((reg) => {
-                console.log('Service Worker is registered', reg);
-            })
+        navigator.serviceWorker
+            .register('/sw.js')
+            .then(() => navigator.serviceWorker.ready)
+            .then((registration) => console.log('Service Worker is registered', registration))
             .catch(err => console.log(`Service Worker failed to register: ${err}`));
     });
 }
